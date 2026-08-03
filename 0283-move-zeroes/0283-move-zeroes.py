4b176d1d588e321a5c6d7e799b9351e48a8f8056
Amrapali Hazra <amrapalihazra983@gmail.com>
2026-08-03 20:50:07 +0530

    Time: 7 ms (55.52%), Space: 13.7 MB (20%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def moveZeroes(self,nums):
+        n = len(nums)
+        j = 0
+        for i in range(n):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
