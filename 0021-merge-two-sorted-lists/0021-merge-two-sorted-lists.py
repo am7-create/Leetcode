acf10795ed0d5f6bbfc6e833c05b5b2b1332281c
Amrapali Hazra <amrapalihazra983@gmail.com>
2026-08-09 10:14:49 +0530

    Time: 3 ms (20.66%), Space: 12.5 MB (47.81%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,10 +9,11 @@ class Solution(object):
         current = dummy
 
         while list1 and list2:
-            if list1.val < list2.val:
+            
+
+            if list1.val<list2.val:
                 current.next = list1
                 list1 = list1.next
-
             else:
                 current.next = list2
                 list2 = list2.next
